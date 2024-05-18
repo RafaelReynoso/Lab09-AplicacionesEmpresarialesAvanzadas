ce1a14b0f8ee6d1417a8532cdd7fd1bb158598d1
Alumno <Alumno@LAB1504-24>
2024-05-18 12:28:52 -0500

    Agregar archivos de proyecto.

---
+++ MVCDemo/Controllers/DemoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+
+    }
+}
